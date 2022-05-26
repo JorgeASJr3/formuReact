f4cec74cfa9ac7bdace8d2cb11140484180faef8
Jorge Júnior <86030512+JorgeASJr3@users.noreply.github.com>
2022-05-26 03:27:14 -0300

    Add files via upload

---
+++ novo-tempo/src/App.tsx
@@ -0,0 +1,14 @@
+import { Header } from './components/Header'
+
+import './styles/header.css'
+import './styles/global.css'
+import { Contact } from './components/Contact'
+
+export function App() {
+  return (
+    <div>
+      {/* <Header /> */}
+      <Contact />
+    </div>
+  )
+}
